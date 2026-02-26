104e67c6a8a1ff4333bb00b9e73bffc86fa2b6e4
Jawwaad <jawwaad.sabree01@gmail.com>
2026-02-26 17:34:01 -0500

    add software

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,9 @@ export default function Home() {
     <div className="grid grid-rows-[100px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="flex flex-col gap-4 items-center grid-row-1">
         <h1 className="text-4xl font-bold">Sabree.dev</h1>
-        <p className="text-lg text-center">Jawwaad Sabree - Developer</p>
+        <p className="text-lg text-center">
+          Jawwaad Sabree - Software Developer
+        </p>
         <div className="flex flex-row">
           <ToggleGroup
             type="single"
